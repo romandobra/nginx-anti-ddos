7cfbc9177727a3d6550be3cc93ecd6ddd1757539
Roman Dobra <romanreachless@gmail.com>
2023-12-22 09:21:14 +0000

    add cooldown

--- nad.sh
+++ nad.sh
@@ -6,24 +6,43 @@ set -e
 
 export NAD_LOG_FILE='access_log'
 export NAD_LINES_TO_CHECK=200
+export NAD_COOLDOWN=300 # seconds
 
 export NAD_DENY_FILE='nad_deny_ip.conf'
 #export NAD_DENY_PAGE='# error_page 403 http://example.com/forbidden.html;'
 
+_NAD_RUNDATE=$(date +%s)
+
+# count log lines
+NAD_LOG_COUNT=($(wc -l access_log | cut -d' ' -f1))
+
+if [ $NAD_LOG_COUNT -gt $NAD_LINES_TO_CHECK ]; then
 # count requests from each IP
-eval "declare -A nad_state=(
-    $(  tail -n$NAD_LINES_TO_CHECK $NAD_LOG_FILE \
-        | cut -d' ' -f1 \
-        | sort \
-        | uniq -c \
-        | while read _number _ip; do echo "[$_ip]=$_number"; done )
-)"
+    eval "declare -A nad_state=(
+        $(  tail -n$NAD_LINES_TO_CHECK $NAD_LOG_FILE \
+            | cut -d' ' -f1 \
+            | sort \
+            | uniq -c \
+            | while read _number _ip
+                do
+                    echo "[$_ip]=$_number"
+                done
+        )
+    )"
+fi
 
 # list blocked
 eval "declare -A nad_blocked=(
     $(  cat $NAD_DENY_FILE \
         | grep -w deny \
-        | while read _ _ip _date; do echo "[${_ip%;}]=${_date//[^[:alnum:]]/}"; done )
+        | while read _ _ip _date
+            do
+                _date=(${_date//[^[:alnum:]]/})
+                if [ $(($_NAD_RUNDATE - $_date )) -lt $NAD_COOLDOWN ]; then
+                    echo "[${_ip%;}]=$_date"
+                fi
+            done
+    )
 )"
 
 echo state ${#nad_state[@]}
@@ -37,7 +56,10 @@ echo blocked ${#nad_blocked[@]}
 
 # check rates
 
-# update deny_ip file
+# remove denied if cooldown is over
 
+
+
+# update deny_ip file
 # reload nginx
 # try first
