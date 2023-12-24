257fd3ec19e856d4a947cae0f2aae960c3bf86fd
Roman Dobra <romanreachless@gmail.com>
2023-12-24 11:37:12 +0000

    rc 0.1

--- nad.sh
+++ nad.sh
@@ -4,11 +4,13 @@ set -e
 
 # TODO prevent multiple instances (pid lock)
 
+export NAD_WHITE_LIST='43.142.47.190'
 export NAD_LOG_FILE='access_log'
-export NAD_LINES_TO_CHECK=200
-#export NAD_MIN_IP_COUNT=5
-export NAD_MAX_REQUESTS=3
-export NAD_COOLDOWN=500 # seconds
+export NAD_LINES_TO_CHECK=100
+export NAD_MAX_REQUESTS=2
+export NAD_COOLDOWN=5 # seconds
+export NAD_LOG_GREP="cat" # for testing
+#export NAD_LOG_GREP="grep -e '$(date '+%d/%b/%Y:%H:%M')' -e '$(date -d 'minute ago' '+%d/%b/%Y:%H:%M')'"
 
 export NAD_DENY_FILE='nad_deny_ip.conf'
 #export NAD_DENY_PAGE='# error_page 403 http://example.com/forbidden.html;'
@@ -20,13 +22,34 @@ nad_report_attack(){ true; }
 
 _NAD_RUNDATE=$(date +%s)
 
+# list blocked
+eval "declare -A nad_blocked=(
+    $(
+        {
+        cat $NAD_DENY_FILE \
+        | grep -w deny \
+        | while read _deny_word _ip _date
+            do
+                _date=(${_date//[^[:alnum:]]/})
+
+# skip if cooldown is over
+                if [ $(( $_NAD_RUNDATE - $_date )) -gt $NAD_COOLDOWN ]; then continue; fi
+
+                echo "[${_ip%;}]=$_date"
+            done
+    } | sort
+    )
+)"
+
 # count log lines
-_NAD_LOG_COUNT=($(wc -l access_log | cut -d' ' -f1))
+_NAD_LOG_COUNT=($(wc -l $NAD_LOG_FILE | cut -d' ' -f1))
 
 if [ $_NAD_LOG_COUNT -gt $NAD_LINES_TO_CHECK ]; then
 # count requests from each IP
     eval "declare -A nad_state=(
-        $(  tail -n$NAD_LINES_TO_CHECK $NAD_LOG_FILE \
+        $({
+        tail -n$NAD_LINES_TO_CHECK $NAD_LOG_FILE \
+            | eval "$NAD_LOG_GREP" \
             | cut -d' ' -f1 \
             | sort \
             | uniq -c \
@@ -34,39 +57,42 @@ if [ $_NAD_LOG_COUNT -gt $NAD_LINES_TO_CHECK ]; then
                 do
 
 # skip if lower than NAD_MAX_REQUESTS
-                    if [ $_number -gt $NAD_MAX_REQUESTS ]; then
-                        echo "[$_ip]=$_number"
-                    fi
+                    if [ $_number -lt $NAD_MAX_REQUESTS ]; then continue; fi
+
+# skip if aready blocked
+                    [ "${nad_blocked[$_ip]+abc}" ] && continue
+#                    if [[ -v nad_blocked[$_ip] ]];then continue; fi
+
+                    echo "[$_ip]=$_number"
                 done
-        )
+        } | sort )
     )"
 fi
 
-# list blocked
-eval "declare -A nad_blocked=(
-    $(  cat $NAD_DENY_FILE \
-        | grep -w deny \
-        | while read _ _ip _date
-            do
-                _date=(${_date//[^[:alnum:]]/})
-
-# remove denied if cooldown is over
-                if [ $(($_NAD_RUNDATE - $_date )) -lt $NAD_COOLDOWN ]; then
-                    echo "[${_ip%;}]=$_date"
-                fi
-            done
-    )
-)"
-
-echo state ${#nad_state[@]}
-echo blocked ${#nad_blocked[@]}
-
-# for i in ${!nad_state[@]}
-# do
-#    echo "$i - ${nad_state[$i]}"
-# done
-# echo total ${#nad_state[@]}
+# echo "# new ${#nad_state[@]}"
+# echo "# old ${#nad_blocked[@]}"
 
 # update deny_ip file
+{
+    echo 'location / {'
+
+    echo "# new ${#nad_state[@]} at $_NAD_RUNDATE"
+    for i in ${!nad_state[@]}; do
+
+# whitelisted
+        if [[ $NAD_WHITE_LIST =~ $i ]]; then
+            echo "# whitelisted $i #$_NAD_RUNDATE"
+        else
+            echo "deny $i #$_NAD_RUNDATE"
+        fi
+    done
+
+    echo "# old ${#nad_blocked[@]}"
+    for i in ${!nad_blocked[@]}; do
+    echo "deny $i #${nad_blocked[$i]}"; done
+
+    echo '}'
+} > $NAD_DENY_FILE
+
 # reload nginx
 # try first
